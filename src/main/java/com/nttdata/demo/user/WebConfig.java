4914c3e7038cc9404fe0761a9d308237db25a0c4
Reinel-Steven <reinelstevenb@gmail.com>
2024-12-16 16:36:47 -0500

    ajuste de crossMapping nuevas urls

--- src/main/java/com/nttdata/demo/user/WebConfig.java
+++ src/main/java/com/nttdata/demo/user/WebConfig.java
@@ -1,6 +1,10 @@
 package com.nttdata.demo.user;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,8 +16,24 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "users-nttdata-front-a2fff0cwchdte2cx.canadacentral-01.azurewebsites.net")
+                .allowedOrigins("http://localhost:4200", "http://localhost:8080",
+                        "https://users-nttdata-front.azurewebsites.net",
+                        "https://users-nttdata-front-docker.azurewebsites.net",
+                        "https://users-nttdata-back-d2chgndrc0dmeba6.canadacentral-01.azurewebsites.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("https://*.azurewebsites.net"); // Admite subdominios
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
     }
 }
